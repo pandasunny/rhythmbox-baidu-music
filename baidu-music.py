3391f1a00e755de1ba8dd786f7b81bac45152d16
pandasunny <pandasunny@gmail.com>
2013-07-08 03:34:56 +0800

    Create the basic content of the main function

--- baidu-music.py
+++ baidu-music.py
@@ -1,1 +1,22 @@
 # -*- coding: utf-8 -*-
+import os
+import rb
+from gi.repository import GObject
+from gi.repository import Gio
+from gi.repository import RB
+from gi.repository import Peas
+from gi.repository import Gtk
+from gi.repository import GdkPixbuf
+
+class BaiduMusicPlugin(GObject.Object, Peas.Activatable):
+    __gtype_name__ = "BaiduMusicPlugin"
+    object = GObject.property(type=GObject.GObject)
+
+    def __init__(self):
+        super(BaiduMusicPlugin, self).__init__()
+
+    def do_activate(self):
+        print "Baidu Music Plugin activated."
+
+    def do_deactivate(self):
+        print "Baidu Music Plugin is deactivated"
