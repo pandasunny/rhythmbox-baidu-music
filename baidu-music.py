385ef5543922b529a42e359730a65e37a80b6e6b
pandasunny <pandasunny@gmail.com>
2013-07-08 19:55:55 +0800

    Create the baidu music entry type and the source.

--- baidu-music.py
+++ baidu-music.py
@@ -7,6 +7,8 @@ from gi.repository import RB
 from gi.repository import Peas
 from gi.repository import Gtk
 from gi.repository import GdkPixbuf
+#from source import BaiduMusicSource
+
 
 class BaiduMusicPlugin(GObject.Object, Peas.Activatable):
     __gtype_name__ = "BaiduMusicPlugin"
@@ -18,5 +20,22 @@ class BaiduMusicPlugin(GObject.Object, Peas.Activatable):
     def do_activate(self):
         print "Baidu Music Plugin activated."
 
+        shell = self.object
+        self.db = shell.props.db
+
+        self.entry_type = BaiduMusicEntryType()
+        self.db.register_entry_type(self.entry_type)
+
     def do_deactivate(self):
         print "Baidu Music Plugin is deactivated"
+
+
+class BaiduMusicEntryType(RB.RhythmDBEntryType):
+
+    def __init__(self):
+        super(BaiduMusicEntryType, self).__init__(
+                name="baidu-music-entry-type"
+                )
+
+    def do_can_sync_metadata(self, entry):
+        return True
--- source.py
+++ source.py
@@ -1,1 +1,35 @@
 # -*- coding: utf-8 -*-
+from gi.repository import GObject
+from gi.repository import RB
+
+class BaiduMusicSource(RB.BrowserSource):
+
+    def __init__(self):
+        super(BaiduMusicSource, self).__init__()
+        self.__activated = False
+
+    def do_selected(self):
+        if not self.__activated:
+            entry_view = self.get_entry_view()
+            entry_view.get_column(RB.EntryViewColumn.TRACK_NUMBER).set_visible(False)
+            entry_view.get_column(RB.EntryViewColumn.GENRE).set_visible(False)
+            entry_view.get_column(RB.EntryViewColumn.DURATION).set_visible(False)
+            self.__activated = True
+
+    def do_add_uri(self):
+        return False
+
+    def do_impl_can_cut(self):
+        return False
+
+    def do_impl_can_copy(self):
+        return False
+
+    def do_impl_can_delete(self):
+        return True
+
+    def do_impl_can_move_to_trash(self):
+        return False
+
+
+GObject.type_register(BaiduMusicSource)
